be028499a59653bf876ce4af40289c4d242338e5
sooryajith <sooryajithakclt@gmail.com>
2023-07-31 10:28:26 +0530

    store and cart functionality

--- storeapp/views.py
+++ storeapp/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
@@ -11,10 +12,16 @@ def store(request,category_slug=None):
     if category_slug != None:
         categories=get_object_or_404(Category,slug=category_slug)
         products = Product.objects.filter(category=categories,is_available=True)
+        paginator = Paginator(products, 4)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
     else:
-        products=Product.objects.all().filter(is_available=True)
+        products=Product.objects.all().filter(is_available=True).order_by('id')
+        paginator=Paginator(products,4)
+        page=request.GET.get('page')
+        paged_product=paginator.get_page(page)
     context={
-        'products':products,
+        'products':paged_product,
     }
     return render(request,'store/store.html',context)
 
